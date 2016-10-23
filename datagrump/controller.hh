60c3839dfeb025352f3d1d0cb0ee2809f4a2e516
Songtao He <songtaohe0@mit.edu>
2016-10-23 00:22:10 -0400

    Pset2 Submit

--- datagrump/controller.hh
+++ datagrump/controller.hh
@@ -2,17 +2,23 @@
 #define CONTROLLER_HH
 
 #include <cstdint>
-
+#include <pthread.h>
 /* Congestion controller interface */
 
 class Controller
 {
 private:
   bool debug_; /* Enables debugging output */
-
-  /* Add member variables here */
-
 public:
+  /* Add member variables here */
+  float window_size_float;
+  float window_size_float_active = 0;
+  float window_size_float_passive = 0;
+  int senddelay = 500;
+
+  double est_time = 0;
+  double link_status = 0;
+  double link_throughput = 0;
   /* Public interface for the congestion controller */
   /* You can change these if you prefer, but will need to change
      the call site as well (in sender.cc) */
@@ -33,9 +39,32 @@ public:
 		     const uint64_t recv_timestamp_acked,
 		     const uint64_t timestamp_ack_received );
 
-  /* How long to wait (in milliseconds) if there are no acks
+  void ack_received_delay_threshold( const uint64_t sequence_number_acked,
+		     const uint64_t send_timestamp_acked,
+		     const uint64_t recv_timestamp_acked,
+		     const uint64_t timestamp_ack_received );
+
+
+  void ack_received_delay_threshold_varied_target( const uint64_t sequence_number_acked,
+		     const uint64_t send_timestamp_acked,
+		     const uint64_t recv_timestamp_acked,
+		     const uint64_t timestamp_ack_received );
+
+  void ack_received_prediction( const uint64_t sequence_number_acked,
+		     const uint64_t send_timestamp_acked,
+		     const uint64_t recv_timestamp_acked,
+		     const uint64_t timestamp_ack_received );
+ 
+ /* How long to wait (in milliseconds) if there are no acks
      before sending one more datagram */
   unsigned int timeout_ms( void );
+
+  int send_counter=0;
+  int recv_counter=0;
+  uint64_t* send_time_list=NULL;
+  uint64_t* recv_time_list=NULL;
+  double* delay_list=NULL;
+
 };
 
 #endif
---
+++ datagrump/parameter.hh
@@ -0,0 +1,6 @@
+int P_WINDOW=128;
+double P_L0=60;
+double P_L1=46;
+double P_L2=74;
+double P_BETA=2;
+double TimeWarpBase=5000;
